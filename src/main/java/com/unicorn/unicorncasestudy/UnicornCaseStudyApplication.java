3f2141ebba46c7b8ea29a7d8817852cd1e10a495
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-04-16 09:49:16 +0200

    initial commit

---
+++ src/main/java/com/unicorn/unicorncasestudy/UnicornCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.unicorn.unicorncasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornCaseStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicorn/unicorncasestudy/UnicornCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicorn.unicorncasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicornCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
