3cd197b5b0616c075155a344e0b9429a73aa9620
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-04-18 10:28:54 +0200

    Reworked Product Service. handleDefinitions method now handles updating derived products correctly. Added exception handling. Updated changeRateAndCreateDerivedProduct to handle changes correctly and also added exception handling.

--- src/main/java/com/unicorn/unicorncasestudy/repositories/ProductRepository.java
+++ src/main/java/com/unicorn/unicorncasestudy/repositories/ProductRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
     boolean existsByProductKey(String productKey);
     Product findByProductKey(String productKey);
     @Query(value = "SELECT * FROM Product p WHERE p.product_key = :productKey AND p.dType = 'Product'", nativeQuery = true)
     Product findByProductKeyForProductType(@Param("productKey") String productKey);
+    @Query(value = "SELECT * FROM Product p WHERE p.product_key = :productKey AND p.dType = 'DerivedProduct'", nativeQuery = true)
+    List<Product> findByProductKeyAndDTypeDerived(@Param("productKey") String productKey);
 }
--- src/main/java/com/unicorn/unicorncasestudy/services/ProductServiceImp.java
+++ src/main/java/com/unicorn/unicorncasestudy/services/ProductServiceImp.java
@@ -7,6 +7,9 @@ import com.unicorn.unicorncasestudy.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductServiceImp implements ProductService {
     private final ProductRepository productRepository;
@@ -15,30 +18,75 @@ public class ProductServiceImp implements ProductService {
     public ProductServiceImp(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-
     @Override
     public void handleDefinitions(DefinitionsRequest request) {
-        for (DefinitionsRequest.ProductDefinitions def : request.getDefinitions()) {
-            if (def.getOperation().equals("N")) {
-                Product product = new Product();
-                product.setProductKey(def.getProductKey());
-                product.setDescription(def.getDescription());
-                product.setType(def.getType());
-                product.setRate(def.getRate());
-                product.setPayRateUnit(def.getPayRate().getUnit());
-                product.setPayRateValue(def.getPayRate().getValue());
-                productRepository.save(product);
-            } else if (def.getOperation().equals("U")) {
-                if (productRepository.existsByProductKey(def.getProductKey())) {
-                    Product product = productRepository.findByProductKey(def.getProductKey());
-                    product.setRate(def.getRate());
-                    product.setPayRateUnit(def.getPayRate().getUnit());
-                    product.setPayRateValue(def.getPayRate().getValue());
-                    productRepository.save(product);
-                } else {
-                    throw new IllegalArgumentException("Product with key " + def.getProductKey() + " does not exist.");
+        List<String> errors = new ArrayList<>();
+        try {
+            for (DefinitionsRequest.ProductDefinitions def : request.getDefinitions()) {
+                if (def.getOperation().equals("N")) {
+                    handleNewProductDefinition(def, errors);
+                } else if (def.getOperation().equals("U")) {
+                    handleUpdatedProductDefinition(def, errors);
                 }
             }
+        } catch (IllegalArgumentException e) {
+            errors.add(e.getMessage());
+        }
+        printErrors(errors);
+    }
+
+    private void handleNewProductDefinition(DefinitionsRequest.ProductDefinitions def, List<String> errors) {
+        if (!productRepository.existsByProductKey(def.getProductKey())) {
+            saveProductFromDefinition(def);
+        } else {
+            errors.add("New - Product with key " + def.getProductKey() + " already exists.");
+        }
+    }
+
+    private void handleUpdatedProductDefinition(DefinitionsRequest.ProductDefinitions def, List<String> errors) {
+        if (productRepository.existsByProductKey(def.getProductKey())) {
+            Product product = productRepository.findByProductKeyForProductType(def.getProductKey());
+            updateProductFromDefinition(product, def);
+            productRepository.save(product);
+            updateDerivedProducts(def);
+        } else {
+            errors.add("Update - Product with key " + def.getProductKey() + " does not exist.");
+        }
+    }
+
+    private void saveProductFromDefinition(DefinitionsRequest.ProductDefinitions def) {
+        Product product = new Product();
+        product.setProductKey(def.getProductKey());
+        product.setDescription(def.getDescription());
+        product.setType(def.getType());
+        product.setRate(def.getRate());
+        product.setPayRateUnit(def.getPayRate().getUnit());
+        product.setPayRateValue(def.getPayRate().getValue());
+        productRepository.save(product);
+    }
+
+    private void updateProductFromDefinition(Product product, DefinitionsRequest.ProductDefinitions def) {
+        product.setRate(def.getRate());
+        product.setPayRateUnit(def.getPayRate().getUnit());
+        product.setPayRateValue(def.getPayRate().getValue());
+    }
+
+    private void updateDerivedProducts(DefinitionsRequest.ProductDefinitions def) {
+        Product mainProduct = productRepository.findByProductKeyForProductType(def.getProductKey());
+        double changeRate = def.getRate() - mainProduct.getRate();
+        List<Product> derivedProducts = productRepository.findByProductKeyAndDTypeDerived(def.getProductKey());
+        for (Product p : derivedProducts) {
+            p.setPayRateUnit(def.getPayRate().getUnit());
+            p.setPayRateValue(def.getPayRate().getValue());
+            double newRate = p.getRate() + changeRate;
+            p.setRate(newRate);
+            productRepository.save(p);
+        }
+    }
+
+    private void printErrors(List<String> errors) {
+        for (String error : errors) {
+            System.err.println(error);
         }
     }
 
@@ -46,17 +94,24 @@ public class ProductServiceImp implements ProductService {
     public void changeRateAndCreateDerivedProduct(String productKey, Double newRate) {
         Product product = productRepository.findByProductKeyForProductType(productKey);
         DerivedProduct derivedProduct = new DerivedProduct(product);
+        List<String> errors = new ArrayList<>();
         try {
             if (product.getType().equals("ACCOUNT")) {
-                if (newRate >= 0 && newRate >= product.getRate() - 250 && newRate <= product.getRate() + 250) derivedProduct.setRate(newRate);
-                else throw new IllegalArgumentException("The new rate does not comply with the modification rules.");
+                if (newRate >= 0 && newRate >= product.getRate() - 250 && newRate <= product.getRate() + 250) {
+                    derivedProduct.setRate(newRate);
+                    productRepository.save(derivedProduct);
+                } else errors.add("The new rate does not comply with the modification rules. Product key: " + productKey + ", old rate: " + product.getRate() + ", new rate: " + newRate);
             } else if (product.getType().equals("LOAN")) {
-                if (newRate >= 0 && newRate >= product.getRate() * 0.8 && newRate <= product.getRate() * 1.2) derivedProduct.setRate(newRate);
-                else throw new IllegalArgumentException("The new rate does not comply with the modification rules.");
+                if (newRate >= 0 && newRate >= product.getRate() * 0.8 && newRate <= product.getRate() * 1.2) {
+                    derivedProduct.setRate(newRate);
+                    productRepository.save(derivedProduct);
+                } else errors.add("The new rate does not comply with the modification rules. Product key: " + productKey + ", old rate: " + product.getRate() + ", new rate: " + newRate);
             }
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
-        productRepository.save(derivedProduct);
+        for (String error : errors) {
+            System.err.println(error);
+        }
     }
 }
