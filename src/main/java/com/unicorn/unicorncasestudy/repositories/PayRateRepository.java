2eb5d8f62d60e1f5e6d86cf98cad96c661060552
Ondrej Jizba <146982321+OndrejJizba@users.noreply.github.com>
2024-04-19 12:43:48 +0200

    Delete PayRateRepository.java

--- src/main/java/com/unicorn/unicorncasestudy/repositories/PayRateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unicorn.unicorncasestudy.repositories;
-
-import com.unicorn.unicorncasestudy.models.PayRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PayRateRepository extends JpaRepository<PayRate, Long> {
-}
